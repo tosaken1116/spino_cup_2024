664114e0aedcd96bbcbae34102576fc92990c276
Kurichi <yuuyapp080@gmail.com>
2024-09-17 08:41:58 +0900

    fix: 文字列""

--- infra/terraform/modules/lb.tf
+++ infra/terraform/modules/lb.tf
@@ -5,7 +5,7 @@ resource "google_compute_backend_bucket" "backend_bucket" {
 }
 
 resource "google_compute_url_map" "url_map" {
-  name            = ${var.common.prefix}-url-map-${var.common.environment}
+  name            = "${var.common.prefix}-url-map-${var.common.environment}"
   default_service = google_compute_backend_bucket.backend_bucket.self_link
 
   # SPAのためにすべてのパスをindex.htmlにリダイレクト
@@ -17,13 +17,13 @@ resource "google_compute_url_map" "url_map" {
 }
 
 resource "google_compute_target_https_proxy" "https_proxy" {
-  name               = ${var.common.prefix}-https-proxy-${var.common.environment}
-  url_map            = google_compute_url_map.url_map.self_link
-  ssl_certificates   = [google_compute_managed_ssl_certificate.web.self_link]
+  name             = "${var.common.prefix}-https-proxy-${var.common.environment}"
+  url_map          = google_compute_url_map.url_map.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.web.self_link]
 }
 
 resource "google_compute_global_forwarding_rule" "forwarding_rule" {
-  name       = ${var.common.prefix}-forwarding-rule-${var.common.environment}
+  name       = "${var.common.prefix}-forwarding-rule-${var.common.environment}"
   ip_address = google_compute_global_address.web.address
   port_range = "443"
   target     = google_compute_target_https_proxy.https_proxy.self_link
