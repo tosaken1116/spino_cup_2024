62855ee7602111fd4e226dc86583a936cdf61b91
tosaken1116 <nagato20021122@gmail.com>
2024-09-24 13:25:08 +0900

    chore: format

--- frontend/src/libs/calculateScreenPosition/index.ts
+++ frontend/src/libs/calculateScreenPosition/index.ts
@@ -1,35 +1,35 @@
 type Props = {
-  max: {
-    alpha: number;
-    beta: number;
-  };
-  min: {
-    alpha: number;
-    beta: number;
-  };
-  center?: {
-    alpha: number;
-    beta: number;
-  };
-  current: {
-    alpha: number;
-    beta: number;
-  };
-  screenSize: {
-    width: number;
-    height: number;
-  };
+	max: {
+		alpha: number;
+		beta: number;
+	};
+	min: {
+		alpha: number;
+		beta: number;
+	};
+	center?: {
+		alpha: number;
+		beta: number;
+	};
+	current: {
+		alpha: number;
+		beta: number;
+	};
+	screenSize: {
+		width: number;
+		height: number;
+	};
 };
 
 export const calculateScreenPosition = (props: Props) => {
-  const { max, min, current, screenSize } = props;
+	const { max, min, current, screenSize } = props;
 
-  const x =
-    (current.alpha / (max.alpha - min.alpha)) * screenSize.width +
-    screenSize.width / 2;
-  const y =
-    (current.beta / (max.beta - min.beta)) * screenSize.height +
-    screenSize.height / 2;
+	const x =
+		(current.alpha / (max.alpha - min.alpha)) * screenSize.width +
+		screenSize.width / 2;
+	const y =
+		(current.beta / (max.beta - min.beta)) * screenSize.height +
+		screenSize.height / 2;
 
-  return { x, y };
+	return { x, y };
 };
