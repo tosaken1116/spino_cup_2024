0c47de73d27d74524218db66eab27295a9be4263
Kurichi <yuuyapp080@gmail.com>
2024-09-24 17:41:39 +0900

    feat: server initialize

--- backend/server.go
+++ backend/server.go
@@ -1,29 +1,25 @@
 package main
 
 import (
-	"net/http"
-	"os"
+	"log"
 
-	"github.com/labstack/echo/v4"
-	"github.com/tosaken1116/spino_cup_2024/backend/pkg/database"
+	"github.com/tosaken1116/spino_cup_2024/backend/internal/app"
 )
 
 func main() {
-	db, err := database.New(&database.Config{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBPort:     os.Getenv("DB_PORT"),
-		DBName:     os.Getenv("DB_NAME"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-	})
+	app, err := app.New()
 	if err != nil {
-		panic(err)
+		log.Printf("failed to create server: %v\n", err)
+		return
 	}
-	defer db.Close()
+	defer func() {
+		if err := app.Close(); err != nil {
+			log.Printf("failed to close server: %v\n", err)
+		}
+	}()
 
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":8080"))
+	if err := app.Start(); err != nil {
+		log.Printf("failed to start server: %v\n", err)
+		return
+	}
 }
