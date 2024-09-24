9cc5650dfab922fd23700e9f3c91e499f038383c
tosaken1116 <nagato20021122@gmail.com>
2024-09-24 18:22:29 +0900

    fix: throw error case of baseUrl

--- frontend/src/libs/baseUrl/index.ts
+++ frontend/src/libs/baseUrl/index.ts
@@ -1,11 +1,11 @@
 const DEV_BASE_URL = import.meta.env.VITE_DEV_BASE_URL;
 const PROD_BASE_URL = import.meta.env.VITE_PROD_BASE_URL;
-
-if (DEV_BASE_URL === undefined) {
+const IS_DEV = import.meta.env.NODE_ENV === "development";
+if (IS_DEV && DEV_BASE_URL === undefined) {
 	throw new Error("DEV_BASE_URL is not defined");
 }
 
-if (PROD_BASE_URL === undefined) {
+if (!IS_DEV && PROD_BASE_URL === undefined) {
 	throw new Error("PROD_BASE_URL is not defined");
 }
 
