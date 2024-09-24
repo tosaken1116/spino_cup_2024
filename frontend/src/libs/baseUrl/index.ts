ec24b8559f8d8dfc595246619b8055abcc7c8493
tosaken1116 <nagato20021122@gmail.com>
2024-09-24 18:20:09 +0900

    fix: get base url env key

--- frontend/src/libs/baseUrl/index.ts
+++ frontend/src/libs/baseUrl/index.ts
@@ -1,5 +1,5 @@
-const DEV_BASE_URL = import.meta.env.VITE_APP_DEV_BASE_URL;
-const PROD_BASE_URL = import.meta.env.VITE_APP_PROD_BASE_URL;
+const DEV_BASE_URL = import.meta.env.VITE_DEV_BASE_URL;
+const PROD_BASE_URL = import.meta.env.VITE_PROD_BASE_URL;
 
 if (DEV_BASE_URL === undefined) {
 	throw new Error("DEV_BASE_URL is not defined");
