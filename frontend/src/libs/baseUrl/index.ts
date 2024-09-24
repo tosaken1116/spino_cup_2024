fc625c9e5e52454ca147bf22a183c5596bfab07d
tosaken1116 <nagato20021122@gmail.com>
2024-09-24 21:37:42 +0900

    chore: change baseUrl lib change by protocol

--- frontend/src/libs/baseUrl/index.ts
+++ frontend/src/libs/baseUrl/index.ts
@@ -1,5 +1,5 @@
-const DEV_BASE_URL = import.meta.env.VITE_DEV_BASE_URL;
-const PROD_BASE_URL = import.meta.env.VITE_PROD_BASE_URL;
+const DEV_BASE_URL = import.meta.env.VITE_DEV_API_DOMAIN;
+const PROD_BASE_URL = import.meta.env.VITE_PROD_API_DOMAIN;
 const IS_DEV = import.meta.env.NODE_ENV === "development";
 if (IS_DEV && DEV_BASE_URL === undefined) {
 	throw new Error("DEV_BASE_URL is not defined");
@@ -9,9 +9,9 @@ if (!IS_DEV && PROD_BASE_URL === undefined) {
 	throw new Error("PROD_BASE_URL is not defined");
 }
 
-export const getBaseUrl = () => {
+export const getBaseUrl = (type: "ws" | "http" = "http"): string => {
 	if (process.env.NODE_ENV === "development") {
-		return DEV_BASE_URL;
+		return `${type}://${DEV_BASE_URL}`;
 	}
-	return PROD_BASE_URL;
+	return `${type}s://${PROD_BASE_URL}`;
 };
