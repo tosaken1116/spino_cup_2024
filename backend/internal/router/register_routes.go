0b5b06b8eff2aa3758a9b71ea9179ef072202ef5
YuyaKurihara <31675622+Kurichi@users.noreply.github.com>
2024-09-24 22:28:38 +0900

    Feature/backend/room domain (#67)
    * feat: room domain model definition
    
    * fix: handler directory path
    
    * feat: definition room usecase
    
    * feat: definition room repository
    
    * fix: join room handler

--- backend/internal/router/register_routes.go
+++ backend/internal/router/register_routes.go
@@ -2,7 +2,7 @@ package router
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/tosaken1116/spino_cup_2024/backend/handler"
+	"github.com/tosaken1116/spino_cup_2024/backend/internal/handler"
 )
 
 func registerRoutes(
@@ -12,5 +12,4 @@ func registerRoutes(
 	e.POST("/v1/rooms", roomHandler.CreateRoom)
 	e.GET("/v1/rooms/:id", roomHandler.GetRoom)
 	e.PUT("/v1/rooms/:id", roomHandler.UpdateRoom)
-	e.POST("/v1/rooms/:id/join", roomHandler.JoinRoom)
 }
