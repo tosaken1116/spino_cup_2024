962bf37291995027e43b76a30ac68a17d70cefd1
tosaken1116 <nagato20021122@gmail.com>
2024-09-24 18:03:46 +0900

    feat: setup msw

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,23 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
 
-// biome-ignore lint/style/noNonNullAssertion: 存在するからnullじゃない
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+enableMocking().then(() => {
+
+	// biome-ignore lint/style/noNonNullAssertion: 存在するからnullじゃない
+	createRoot(document.getElementById("root")!).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+})
