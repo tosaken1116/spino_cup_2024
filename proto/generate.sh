3dc9440f05afa93a187a29cd7e32dc33fc3a8550
tosaken1116 <nagato20021122@gmail.com>
2024-09-24 11:23:26 +0900

    feat: add ws client generate plugin

--- proto/generate.sh
+++ proto/generate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "generating with using buf.gen.ws.yaml"
+buf generate --template buf.gen.ws.yaml
+echo "finish generate with using buf.gen.ws.yaml"
+
 echo "generating with using buf.gen.api.yaml"
 buf generate --template buf.gen.api.yaml
 echo "finish generate with using buf.gen.api.yaml"
