0ca07570110a13aa6dc0f0414a674aaba30cee63
YuyaKurihara <31675622+Kurichi@users.noreply.github.com>
2024-09-24 19:42:49 +0900

    Feature/backend/migrations (#58)
    * feat: migrations for rooms
    
    * feat: definition room handler

---
+++ backend/handler/room_handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+type RoomHandler interface {
+	CreateRoom(c echo.Context) error
+	GetRoom(c echo.Context) error
+	UpdateRoom(c echo.Context) error
+	JoinRoom(c echo.Context) error
+}
+
+type roomHandler struct {
+}
+
+func NewRoomHandler() RoomHandler {
+	return &roomHandler{}
+}
+
+// CreateRoom implements RoomHandler.
+func (r *roomHandler) CreateRoom(c echo.Context) error {
+	panic("unimplemented")
+}
+
+// GetRoom implements RoomHandler.
+func (r *roomHandler) GetRoom(c echo.Context) error {
+	panic("unimplemented")
+}
+
+// JoinRoom implements RoomHandler.
+func (r *roomHandler) JoinRoom(c echo.Context) error {
+	panic("unimplemented")
+}
+
+// UpdateRoom implements RoomHandler.
+func (r *roomHandler) UpdateRoom(c echo.Context) error {
+	panic("unimplemented")
+}
