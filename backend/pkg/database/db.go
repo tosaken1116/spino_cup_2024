2760d44b226623ef0071665f689f62a4d365a30f
YuyaKurihara <31675622+Kurichi@users.noreply.github.com>
2024-09-24 14:40:20 +0900

    feat: secret (#41)

---
+++ backend/pkg/database/db.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/mysqldialect"
+)
+
+type DB struct {
+	*bun.DB
+}
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBName     string
+	DBUser     string
+	DBPassword string
+}
+
+func New(cfg *Config) (*DB, error) {
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
+
+	sqldb, err := sql.Open("mysql", dns)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open mysql connection: %w", err)
+	}
+
+	db := bun.NewDB(sqldb, mysqldialect.New())
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping mysql: %w", err)
+	}
+
+	return &DB{db}, nil
+}
--- backend/server.go
+++ backend/server.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/tosaken1116/spino_cup_2024/backend/pkg/database"
 )
 
 func main() {
+	db, err := database.New(&database.Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
