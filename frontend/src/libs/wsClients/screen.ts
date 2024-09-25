b0dcc691fb19cdbcb6624a5fb0ddb2e004143321
tosaken1116 <nagato20021122@gmail.com>
2024-09-25 11:01:20 +0900

    fix: by ws client changes

--- frontend/src/libs/wsClients/screen.ts
+++ frontend/src/libs/wsClients/screen.ts
@@ -4,16 +4,17 @@ import type { UserPosition } from "../../generated/wsClient/room/model";
 import { getBaseUrl } from "../baseUrl";
 
 export const useScreenUserWSClient = (roomId: string) => {
-	const baseUrl = getBaseUrl("ws");
-	const [positions, setPositions] = useState<UserPosition[]>([]);
-	useRoomWSClient({
-		baseUrl: `${baseUrl}/rooms/${roomId}`,
-		ChangeCurrentPosition: () => {},
-		ChangeCurrentScreen: () => {},
-		ChangeUserPosition: (data) => {
-			setPositions(data.userPositions);
-		},
-	});
+  const baseUrl = getBaseUrl("ws");
+  const [positions, setPositions] = useState<UserPosition[]>([]);
+  useRoomWSClient({
+    baseUrl: `${baseUrl}/rooms/${roomId}/join`,
+    ChangeUserPosition: (payload) => {
+      setPositions(payload.payload);
+    },
+    onChangeCurrentPosition: () => {},
+    onChangeCurrentScreen: () => {},
+    JoinRoom: () => {},
+  });
 
-	return { positions };
+  return { positions };
 };
--- frontend/src/libs/wsClients/user.ts
+++ frontend/src/libs/wsClients/user.ts
@@ -4,37 +4,37 @@ import type { UserPosition } from "../../generated/wsClient/room/model";
 import { getBaseUrl } from "../baseUrl";
 
 export const useRoomUserWSClient = (roomId: string) => {
-	const baseUrl = getBaseUrl("ws");
-	const [position, setPosition] = useState<UserPosition>({
-		x: 0,
-		y: 0,
-		z: 0,
-		color: "#000000",
-		isClicked: false,
-		id: "test-id",
-	});
-	const { connection } = useRoomWSClient({
-		baseUrl: `${baseUrl}/rooms/${roomId}/join`,
-		ChangeCurrentPosition: () => {},
-		ChangeCurrentScreen: () => {},
-		ChangeUserPosition: () => {},
-	});
+  const baseUrl = getBaseUrl("ws");
+  const [position, setPosition] = useState<UserPosition>({
+    x: 0,
+    y: 0,
+    color: "#000000",
+    isClicked: false,
+    id: "test-id",
+  });
+  const { connection } = useRoomWSClient({
+    baseUrl: `${baseUrl}/rooms/${roomId}/join`,
+    ChangeUserPosition: () => {},
+    onChangeCurrentPosition: () => {},
+    onChangeCurrentScreen: () => {},
+    JoinRoom: () => {},
+  });
 
-	const handleClick = () => {
-		if (connection) {
-			connection.send(
-				JSON.stringify({
-					type: "ChangeUserPosition",
-					payload: {
-						...position,
-					},
-				}),
-			);
-			setPosition((prev) => ({
-				...prev,
-				isClicked: !prev.isClicked,
-			}));
-		}
-	};
-	return { handleClick, position, isConnected: connection?.CONNECTING };
+  const handleClick = () => {
+    if (connection) {
+      connection.send(
+        JSON.stringify({
+          type: "ChangeUserPosition",
+          payload: {
+            ...position,
+          },
+        })
+      );
+      setPosition((prev) => ({
+        ...prev,
+        isClicked: !prev.isClicked,
+      }));
+    }
+  };
+  return { handleClick, position, isConnected: connection?.CONNECTING };
 };
