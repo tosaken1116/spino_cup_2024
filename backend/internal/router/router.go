c265974908ac99cd7d2a1551af8a7ce682bac105
Kurichi <yuuyapp080@gmail.com>
2024-09-25 13:07:43 +0900

    feat: db&echo instrument

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -7,6 +7,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/tosaken1116/spino_cup_2024/backend/internal/handler"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
 )
 
 func New(roomHandler handler.RoomHandler, activeRoomHandler handler.WSHandler) *echo.Echo {
@@ -29,6 +30,9 @@ func setup(e *echo.Echo) {
 			return c.Request().URL.Path == "/"
 		},
 	}))
+	e.Use(otelecho.Middleware("api.spino.kurichi.dev", otelecho.WithSkipper(func(c echo.Context) bool {
+		return c.Request().URL.Path == "/" || c.Request().URL.Path == "/rooms/:id/join"
+	})))
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		Skipper: func(c echo.Context) bool {
