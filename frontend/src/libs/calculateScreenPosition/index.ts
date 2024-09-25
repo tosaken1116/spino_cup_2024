3576dc45faebaa366f128d73d53b8a4e9ca69371
tosaken1116 <nagato20021122@gmail.com>
2024-09-25 23:04:23 +0900

    fix: eventlistener timing

--- frontend/src/libs/calculateScreenPosition/index.ts
+++ frontend/src/libs/calculateScreenPosition/index.ts
@@ -31,35 +31,27 @@ type Props = {
 //   const y = (max.beta - current.beta) / (max.beta - min.beta);
 //   return { x: 2 * (1 - x), y: 1 - y };
 // };
-
 function normalizeAngle(angle: number): number {
 	return (angle + 360) % 360;
 }
 
-function angleDifference(angle1: number, angle2: number): number {
-	const normalizedAngle1 = normalizeAngle(angle1);
-	const normalizedAngle2 = normalizeAngle(angle2);
-	const diff = normalizeAngle(normalizedAngle2 - normalizedAngle1);
+function angleDifference(minAngle: number, maxAngle: number): number {
+	const diff = normalizeAngle(maxAngle - minAngle);
 	return diff > 180 ? diff - 360 : diff;
 }
 
-const calculateAngleRatio = (
-	angle1: number,
-	angle2: number,
+function calculateAnglePosition(
+	minAngle: number,
+	maxAngle: number,
 	targetAngle: number,
-): number => {
-	// 角度を正規化
-	const normalizedAngle1 = normalizeAngle(angle1);
-	const normalizedAngle2 = normalizeAngle(angle2);
-	const normalizedTargetAngle = normalizeAngle(targetAngle);
+): number {
+	const normalizedMin = normalizeAngle(minAngle);
+	const normalizedMax = normalizeAngle(maxAngle);
+	const normalizedTarget = normalizeAngle(targetAngle);
 
-	// 角度差を計算
-	const totalDiff = Math.abs(
-		angleDifference(normalizedAngle1, normalizedAngle2),
-	);
+	const totalDiff = Math.abs(angleDifference(normalizedMin, normalizedMax));
 
-	// angle1とtargetAngleの間の差を計算
-	let targetDiff = angleDifference(normalizedAngle1, normalizedTargetAngle);
+	let targetDiff = angleDifference(normalizedMin, normalizedTarget);
 	if (targetDiff < 0) {
 		targetDiff += 360; // 負の差を修正
 	}
@@ -67,20 +59,20 @@ const calculateAngleRatio = (
 	const ratio = targetDiff / totalDiff;
 
 	return ratio;
-};
+}
 export const calculateScreenPosition = (props: Props) => {
-	const x = calculateAngleRatio(
+	const x = calculateAnglePosition(
 		props.min.alpha,
 		props.max.alpha,
 		props.current.alpha,
 	);
-	const y = calculateAngleRatio(
+	const y = calculateAnglePosition(
 		props.min.beta,
 		props.max.beta,
 		props.current.beta,
 	);
 	return {
-		x: Math.max(Math.min(Math.round(x * 100) / 100, 1), 0),
-		y: Math.max(Math.min(Math.round(y * 100) / 100, 1), 0),
+		x: Math.max(Math.min(1 - Math.round(x * 100) / 100, 1), 0),
+		y: Math.max(Math.min(1 - Math.round(y * 100) / 100, 1), 0),
 	};
 };
