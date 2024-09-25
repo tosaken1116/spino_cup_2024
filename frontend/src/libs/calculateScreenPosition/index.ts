31e391fd5dc5ddd0b2050ca455fd861be0c81ddb
tosaken1116 <nagato20021122@gmail.com>
2024-09-25 19:56:29 +0900

    chore: apply current pos to client tablet

--- frontend/src/libs/calculateScreenPosition/index.ts
+++ frontend/src/libs/calculateScreenPosition/index.ts
@@ -30,7 +30,7 @@ export const calculateScreenPosition = (props: Props) => {
 		(current.alpha + (current.alpha < 180 ? add : 0) - right) / (left - right);
 	const y = (max.beta - current.beta) / (max.beta - min.beta);
 	return {
-		x: Math.min(Math.max(2 * (1 - x), 0), 1),
-		y: Math.min(Math.max(1 - y, 0), 1),
+		x: Math.floor(Math.min(Math.max(2 * (1 - x), 0), 1) * 100) / 100,
+		y: Math.floor(Math.min(Math.max(1 - y, 0), 1) * 100) / 100,
 	};
 };
