3421f64d57e3e217d8ede1329c2b5a72e3134327
Kurichi <yuuyapp080@gmail.com>
2024-09-25 20:50:43 +0900

    feat: room usecase test

--- backend/internal/usecase/room_usecase_test.go
+++ backend/internal/usecase/room_usecase_test.go
@@ -79,25 +79,88 @@ func TestNewRoomUsecase(t *testing.T) {
 }
 
 func Test_roomUsecase_CreateRoom(t *testing.T) {
-	type fields struct {
-	}
+	t.Parallel()
+
 	type args struct {
 		ctx context.Context
 		dto *RoomDTO
 	}
 	tests := []struct {
 		name      string
-		fields    fields
 		args      args
+		fn        func(repo *mockrepo.MockRoomRepository)
 		want      *RoomDTO
 		assertion assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				ctx: context.TODO(),
+				dto: &RoomDTO{
+					Name:        "room1",
+					Description: "room1 description",
+				},
+			},
+			fn: func(repo *mockrepo.MockRoomRepository) {
+				repo.EXPECT().CreateRoom(gomock.Any(), gomock.Any()).Return(nil)
+			},
+			want: &RoomDTO{
+				Name:        "room1",
+				Description: "room1 description",
+			},
+			assertion: assert.NoError,
+		},
+		{
+			name: "failed (name required)",
+			args: args{
+				ctx: context.TODO(),
+				dto: &RoomDTO{
+					Name:        "",
+					Description: "room1 description",
+				},
+			},
+			fn:   func(repo *mockrepo.MockRoomRepository) {},
+			want: nil,
+			assertion: func(t assert.TestingT, err error, _ ...interface{}) bool {
+				return assert.ErrorIs(t, err, model.ErrRoomNameRequired)
+			},
+		},
+		{
+			name: "failed (repo error)",
+			args: args{
+				ctx: context.TODO(),
+				dto: &RoomDTO{
+					Name:        "room1",
+					Description: "room1 description",
+				},
+			},
+			fn: func(repo *mockrepo.MockRoomRepository) {
+				repo.EXPECT().CreateRoom(gomock.Any(), gomock.Any()).Return(assert.AnError)
+			},
+			want:      nil,
+			assertion: assert.Error,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := &roomUsecase{}
+			t.Parallel()
+
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			repo := mockrepo.NewMockRoomRepository(mockCtrl)
+			tt.fn(repo)
+
+			r := &roomUsecase{
+				repo: repo,
+			}
 			got, err := r.CreateRoom(tt.args.ctx, tt.args.dto)
+			if got != nil {
+				got.ID = tt.want.ID
+				got.CreatedAt = time.Time{}
+				got.UpdatedAt = time.Time{}
+			}
+
 			tt.assertion(t, err)
 			assert.Equal(t, tt.want, got)
 		})
@@ -189,20 +252,77 @@ func Test_roomUsecase_GetRoom(t *testing.T) {
 }
 
 func Test_roomUsecase_ListRoom(t *testing.T) {
+	t.Parallel()
+
+	id, _ := model.NewRoomID()
 	type args struct {
 		ctx context.Context
 	}
 	tests := []struct {
 		name      string
 		args      args
+		fn        func(repo *mockrepo.MockRoomRepository)
 		want      []*RoomDTO
 		assertion assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				ctx: context.TODO(),
+			},
+			fn: func(repo *mockrepo.MockRoomRepository) {
+				repo.EXPECT().ListRoom(gomock.Any()).Return([]*model.Room{
+					{
+						ID:          id,
+						Name:        "room1",
+						Description: "room1 description",
+					},
+					{
+						ID:          id,
+						Name:        "room2",
+						Description: "room2 description",
+					},
+				}, nil)
+			},
+			want: []*RoomDTO{
+				{
+					ID:          id.String(),
+					Name:        "room1",
+					Description: "room1 description",
+				},
+				{
+					ID:          id.String(),
+					Name:        "room2",
+					Description: "room2 description",
+				},
+			},
+			assertion: assert.NoError,
+		},
+		{
+			name: "failed (repo error)",
+			args: args{
+				ctx: context.TODO(),
+			},
+			fn: func(repo *mockrepo.MockRoomRepository) {
+				repo.EXPECT().ListRoom(gomock.Any()).Return(nil, assert.AnError)
+			},
+			want:      nil,
+			assertion: assert.Error,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := &roomUsecase{}
+			t.Parallel()
+
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			repo := mockrepo.NewMockRoomRepository(mockCtrl)
+			tt.fn(repo)
+
+			r := &roomUsecase{
+				repo: repo,
+			}
 			got, err := r.ListRoom(tt.args.ctx)
 			tt.assertion(t, err)
 			assert.Equal(t, tt.want, got)
@@ -211,25 +331,123 @@ func Test_roomUsecase_ListRoom(t *testing.T) {
 }
 
 func Test_roomUsecase_UpdateRoom(t *testing.T) {
-	type fields struct {
-	}
+	t.Parallel()
+
+	rawid := ulid.MustNew(uint64(time.Now().UnixMilli()), nil).String()
 	type args struct {
 		ctx context.Context
 		dto *RoomDTO
 	}
 	tests := []struct {
 		name      string
-		fields    fields
 		args      args
+		fn        func(repo *mockrepo.MockRoomRepository)
 		want      *RoomDTO
 		assertion assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				ctx: context.TODO(),
+				dto: &RoomDTO{
+					ID:          rawid,
+					Name:        "room1",
+					Description: "room1 description",
+				},
+			},
+			fn: func(repo *mockrepo.MockRoomRepository) {
+				id, _ := model.ParseRoomID(rawid)
+				repo.EXPECT().GetRoom(gomock.Any(), id).Return(&model.Room{
+					ID:          id,
+					Name:        "room1",
+					Description: "room1 description",
+				}, nil)
+				repo.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).Return(nil)
+			},
+			want: &RoomDTO{
+				ID:          rawid,
+				Name:        "room1",
+				Description: "room1 description",
+			},
+			assertion: assert.NoError,
+		},
+		{
+			name: "failed (invalid id)",
+			args: args{
+				ctx: context.TODO(),
+				dto: &RoomDTO{
+					ID:          "invalid",
+					Name:        "room1",
+					Description: "room1 description",
+				},
+			},
+			fn:   func(repo *mockrepo.MockRoomRepository) {},
+			want: nil,
+			assertion: func(t assert.TestingT, err error, _ ...interface{}) bool {
+				return assert.ErrorIs(t, err, model.ErrRoomIDInvalid)
+			},
+		},
+		{
+			name: "failed (repo.Get error)",
+			args: args{
+				ctx: context.TODO(),
+				dto: &RoomDTO{
+					ID:          rawid,
+					Name:        "room1",
+					Description: "room1 description",
+				},
+			},
+			fn: func(repo *mockrepo.MockRoomRepository) {
+				id, _ := model.ParseRoomID(rawid)
+				repo.EXPECT().GetRoom(gomock.Any(), id).Return(nil, model.ErrRoomNotFound)
+			},
+			want: nil,
+			assertion: func(t assert.TestingT, err error, _ ...interface{}) bool {
+				return assert.ErrorIs(t, err, model.ErrRoomNotFound)
+			},
+		},
+		{
+			name: "failed (repo.Update error)",
+			args: args{
+				ctx: context.TODO(),
+				dto: &RoomDTO{
+					ID:          rawid,
+					Name:        "room1",
+					Description: "room1 description",
+				},
+			},
+			fn: func(repo *mockrepo.MockRoomRepository) {
+				id, _ := model.ParseRoomID(rawid)
+				repo.EXPECT().GetRoom(gomock.Any(), id).Return(&model.Room{
+					ID:          id,
+					Name:        "room1",
+					Description: "room1 description",
+				}, nil)
+				repo.EXPECT().UpdateRoom(gomock.Any(), gomock.Any()).Return(assert.AnError)
+			},
+			want:      nil,
+			assertion: assert.Error,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := &roomUsecase{}
+			t.Parallel()
+
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			repo := mockrepo.NewMockRoomRepository(mockCtrl)
+			tt.fn(repo)
+
+			r := &roomUsecase{
+				repo: repo,
+			}
 			got, err := r.UpdateRoom(tt.args.ctx, tt.args.dto)
+			if got != nil {
+				got.CreatedAt = time.Time{}
+				got.UpdatedAt = time.Time{}
+			}
+
 			tt.assertion(t, err)
 			assert.Equal(t, tt.want, got)
 		})
