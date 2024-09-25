21b81c20ef70958c0eb94fb5747843d8eb9c6081
tosaken1116 <nagato20021122@gmail.com>
2024-09-25 11:10:03 +0900

    chore:format frontend

--- frontend/src/libs/wsClients/screen.ts
+++ frontend/src/libs/wsClients/screen.ts
@@ -4,17 +4,17 @@ import type { UserPosition } from "../../generated/wsClient/room/model";
 import { getBaseUrl } from "../baseUrl";
 
 export const useScreenUserWSClient = (roomId: string) => {
-  const baseUrl = getBaseUrl("ws");
-  const [positions, setPositions] = useState<UserPosition[]>([]);
-  useRoomWSClient({
-    baseUrl: `${baseUrl}/rooms/${roomId}/join`,
-    ChangeUserPosition: (payload) => {
-      setPositions(payload.payload);
-    },
-    onChangeCurrentPosition: () => {},
-    onChangeCurrentScreen: () => {},
-    JoinRoom: () => {},
-  });
+	const baseUrl = getBaseUrl("ws");
+	const [positions, setPositions] = useState<UserPosition[]>([]);
+	useRoomWSClient({
+		baseUrl: `${baseUrl}/rooms/${roomId}/join`,
+		ChangeUserPosition: (payload) => {
+			setPositions(payload.payload);
+		},
+		onChangeCurrentPosition: () => {},
+		onChangeCurrentScreen: () => {},
+		JoinRoom: () => {},
+	});
 
-  return { positions };
+	return { positions };
 };
