cc26e834667194c553fd082ceb136eb2cb342ba9
tosaken1116 <nagato20021122@gmail.com>
2024-09-25 17:42:05 +0900

    fix: calculate screen position utils

--- frontend/src/libs/calculateScreenPosition/index.ts
+++ frontend/src/libs/calculateScreenPosition/index.ts
@@ -1,32 +1,38 @@
 type Props = {
-	max: {
-		alpha: number;
-		beta: number;
-	};
-	min: {
-		alpha: number;
-		beta: number;
-	};
-	center?: {
-		alpha: number;
-		beta: number;
-	};
-	current: {
-		alpha: number;
-		beta: number;
-	};
-	screenSize: {
-		width: number;
-		height: number;
-	};
+  max: {
+    alpha: number;
+    beta: number;
+  };
+  min: {
+    alpha: number;
+    beta: number;
+  };
+  center?: {
+    alpha: number;
+    beta: number;
+  };
+  current: {
+    alpha: number;
+    beta: number;
+  };
+  screenSize: {
+    width: number;
+    height: number;
+  };
 };
 
 export const calculateScreenPosition = (props: Props) => {
-	const { max, min, current } = props;
+  const { max, min, current } = props;
 
-	const x =
-		Math.abs(max.alpha - current.alpha) / Math.abs(max.alpha - min.alpha);
-	const y = Math.abs(max.beta - current.beta) / Math.abs(max.beta - min.beta);
+  const x = Math.tan(
+    (Math.abs(max.alpha - current.alpha) / Math.abs(max.alpha - min.alpha)) *
+      Math.PI
+  );
 
-	return { x: Math.min(Math.max(x, 0), 1), y: Math.min(Math.max(y, 0), 1) };
+  const y = Math.tan(
+    (Math.abs(max.beta - current.beta) / Math.abs(max.beta - min.beta)) *
+      Math.PI
+  );
+
+  return { x: Math.min(Math.max(x, 0), 1), y: Math.min(Math.max(y, 0), 1) };
 };
