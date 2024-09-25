1fcccb1c11d3972c215b2ffa97dad07d083d7bb4
tosaken1116 <nagato20021122@gmail.com>
2024-09-25 10:02:51 +0900

    fix: ws path

--- frontend/src/libs/wsClients/screen.ts
+++ frontend/src/libs/wsClients/screen.ts
@@ -4,16 +4,16 @@ import type { UserPosition } from "../../generated/wsClient/room/model";
 import { getBaseUrl } from "../baseUrl";
 
 export const useScreenUserWSClient = (roomId: string) => {
-	const baseUrl = getBaseUrl("ws");
-	const [positions, setPositions] = useState<UserPosition[]>([]);
-	useRoomWSClient({
-		baseUrl: `${baseUrl}/room/${roomId}`,
-		ChangeCurrentPosition: () => {},
-		ChangeCurrentScreen: () => {},
-		ChangeUserPosition: (data) => {
-			setPositions(data.userPositions);
-		},
-	});
+  const baseUrl = getBaseUrl("ws");
+  const [positions, setPositions] = useState<UserPosition[]>([]);
+  useRoomWSClient({
+    baseUrl: `${baseUrl}/rooms/${roomId}`,
+    ChangeCurrentPosition: () => {},
+    ChangeCurrentScreen: () => {},
+    ChangeUserPosition: (data) => {
+      setPositions(data.userPositions);
+    },
+  });
 
-	return { positions };
+  return { positions };
 };
