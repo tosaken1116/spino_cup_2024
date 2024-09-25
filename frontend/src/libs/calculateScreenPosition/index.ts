41cc79c6c6686c8e9649bb59848b3f853f98aa63
tosaken1116 <nagato20021122@gmail.com>
2024-09-25 20:27:42 +0900

    chore: revert calculate case

--- frontend/src/libs/calculateScreenPosition/index.ts
+++ frontend/src/libs/calculateScreenPosition/index.ts
@@ -29,5 +29,5 @@ export const calculateScreenPosition = (props: Props) => {
 	const x =
 		(current.alpha + (current.alpha < 180 ? add : 0) - right) / (left - right);
 	const y = (max.beta - current.beta) / (max.beta - min.beta);
-	return { x: 1 - x, y: 1 - y };
+	return { x: 2 * (1 - x), y: 1 - y };
 };
