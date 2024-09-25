fa827a67520a0aa17307b03c841ee9503b8b99a0
tosaken1116 <nagato20021122@gmail.com>
2024-09-25 18:55:43 +0900

    chore: format

--- frontend/src/libs/calculateScreenPosition/index.ts
+++ frontend/src/libs/calculateScreenPosition/index.ts
@@ -1,36 +1,36 @@
 type Props = {
-  max: {
-    alpha: number;
-    beta: number;
-  };
-  min: {
-    alpha: number;
-    beta: number;
-  };
-  center?: {
-    alpha: number;
-    beta: number;
-  };
-  current: {
-    alpha: number;
-    beta: number;
-  };
-  screenSize: {
-    width: number;
-    height: number;
-  };
+	max: {
+		alpha: number;
+		beta: number;
+	};
+	min: {
+		alpha: number;
+		beta: number;
+	};
+	center?: {
+		alpha: number;
+		beta: number;
+	};
+	current: {
+		alpha: number;
+		beta: number;
+	};
+	screenSize: {
+		width: number;
+		height: number;
+	};
 };
 
 export const calculateScreenPosition = (props: Props) => {
-  const { max, min, current } = props;
-  const add = max.alpha < min.alpha ? 360 : 0;
-  const left = max.alpha + add;
-  const right = min.alpha;
-  const x =
-    (current.alpha + (current.alpha < 180 ? add : 0) - right) / (left - right);
-  const y = (max.beta - current.beta) / (max.beta - min.beta);
-  return {
-    x: Math.min(Math.max(2 * (1 - x), 0), 1),
-    y: Math.min(Math.max(1 - y, 0), 1),
-  };
+	const { max, min, current } = props;
+	const add = max.alpha < min.alpha ? 360 : 0;
+	const left = max.alpha + add;
+	const right = min.alpha;
+	const x =
+		(current.alpha + (current.alpha < 180 ? add : 0) - right) / (left - right);
+	const y = (max.beta - current.beta) / (max.beta - min.beta);
+	return {
+		x: Math.min(Math.max(2 * (1 - x), 0), 1),
+		y: Math.min(Math.max(1 - y, 0), 1),
+	};
 };
