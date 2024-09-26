e1f9e45fd4367ef6a68a59866c33c2c2f97e2845
tosaken1116 <nagato20021122@gmail.com>
2024-09-26 14:04:33 +0900

    chore: change some layout of top page

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,16 +1,21 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
-	component: () => (
-		<div>
-			<header className="h-12">
-				<Link to="/" className="[&.active]:font-bold">
-					お絵描きの谷
-				</Link>
-			</header>
-			<div className="w-full h-[calc(100vh-48px)]">
-				<Outlet />
-			</div>
-		</div>
-	),
+  component: () => (
+    <div className="w-screen overflow-x-hidden">
+      <header className="h-10 px-4 py-2">
+        <Link to="/" className="[&.active]:font-bold flex flex-row items-end">
+          <p className="text-blue-400 text-4xl rotate-2">お</p>
+          <p className="text-red-400 text-5xl -rotate-2">絵</p>
+          <p className="text-orange-400 text-3xl rotate-3">描</p>
+          <p className="text-green-400 text-4xl rotate-6">き</p>
+          <p className="text-2xl">の</p>
+          <p className="text-blue-400 text-5xl">谷</p>
+        </Link>
+      </header>
+      <div className="w-full h-[calc(100vh-48px)]">
+        <Outlet />
+      </div>
+    </div>
+  ),
 });
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,8 +13,20 @@ const config: Config = {
             opacity: "1",
           },
         },
+        jump: {
+          "0%": {
+            transform: "translateY(0px)",
+          },
+          "50%": {
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            transform: "translateY(0px)",
+          },
+        },
       },
       animation: {
+        jump: "jump 0.5s ease-in-out 1",
         "fade-in": "fade-in 0.5s ease-in-out backwards",
       },
     },
